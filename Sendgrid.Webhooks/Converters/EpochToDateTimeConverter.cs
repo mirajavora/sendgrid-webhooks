096ad74c3fd3d878a6d51cb830917a172dccb3c6
Mira Javora <mira.javora@skyscanner.net>
2015-05-11 21:32:46 +0100

    Deal with timestamp - add epoch converter

---
+++ Sendgrid.Webhooks/Converters/EpochToDateTimeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Sendgrid.Webhooks.Converters
+{
+    public class EpochToDateTimeConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(DateTime);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            long timestamp = (long) reader.Value;
+
+            return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(timestamp);
+        }
+    }
+}
--- Sendgrid.Webhooks/Events/WebhookEventBase.cs
+++ Sendgrid.Webhooks/Events/WebhookEventBase.cs
@@ -1,5 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
 using System.Collections.Generic;
+using Sendgrid.Webhooks.Converters;
 using Sendgrid.Webhooks.Service;
 
 namespace Sendgrid.Webhooks.Events
@@ -14,5 +16,8 @@ namespace Sendgrid.Webhooks.Events
 
         [JsonProperty("category"), JsonConverter(typeof(WebhookCategoryConverter))]
         public IList<string> Category { get; set; }
+
+        [JsonProperty("timestamp"), JsonConverter(typeof(EpochToDateTimeConverter))]
+        public DateTime TimeStamp { get; set; }
     }
 }
