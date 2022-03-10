c23b332052b6b4a181dbdffed0106575e13c7236
Joe de Ronde <joe@regtransfers.co.uk>
2022-03-10 14:16:27 +0000

    added the sg_machine_open bool value from SendGrid to track if Apple are artifically inflating the opens

--- Sendgrid.Webhooks/Events/OpenEvent.cs
+++ Sendgrid.Webhooks/Events/OpenEvent.cs
@@ -1,7 +1,12 @@
-﻿namespace Sendgrid.Webhooks.Events
+﻿using Newtonsoft.Json;
+using Sendgrid.Webhooks.Converters;
+
+namespace Sendgrid.Webhooks.Events
 {
     public class OpenEvent : EngagementEventBase
     {
-         
+        [JsonConverter(typeof(BooleanConverter))]
+        [JsonProperty("sg_machine_open")]
+        public bool SgMachineOpen { get; set; } = false;
     }
 }
